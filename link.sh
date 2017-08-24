fb12c73e7984ff7923029a54c6eb8366787fd3d9
bmorcos <morcos.ben@gmail.com>
2017-08-24 10:07:36 -0400

    First commit

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf $PWD/alias $HOME/.alias
+ln -sf $PWD/sshconfig $HOME/.ssh/config
+ln -sf $PWD/gitconfig $HOME/.gitconfig
+ln -sf $PWD/ordering-override.keyfile /usr/share/indicator-application/ordering-override.keyfile
+ln -sf $PWD/chrome-remote-desktop-session $HOME/.chrome-remote-desktop-session
+ln -sf $PWD/chrome-remote-desktop /opt/google/chrome-remote-desktop/chrome-remote-desktop
