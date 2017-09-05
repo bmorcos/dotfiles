357ec642c5787598da512db7a82d75f383cef3ff
bmorcos <morcos.ben@gmail.com>
2017-09-05 10:14:06 -0400

    Added tmux

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 ln -sf $PWD/alias $HOME/.alias
 ln -sf $PWD/sshconfig $HOME/.ssh/config
 ln -sf $PWD/gitconfig $HOME/.gitconfig
+ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/ordering-override.keyfile /usr/share/indicator-application/ordering-override.keyfile
 ln -sf $PWD/chrome-remote-desktop-session $HOME/.chrome-remote-desktop-session
 ln -sf $PWD/chrome-remote-desktop /opt/google/chrome-remote-desktop/chrome-remote-desktop
