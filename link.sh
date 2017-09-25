205caa4e80ea39b26ca6f42d01b826a9040ed0c2
bmorcos <morcos.ben@gmail.com>
2017-09-25 10:47:42 -0400

    Added Sublime Text configs

--- link.sh
+++ link.sh
@@ -7,3 +7,5 @@ ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/ordering-override.keyfile /usr/share/indicator-application/ordering-override.keyfile
 ln -sf $PWD/chrome-remote-desktop-session $HOME/.chrome-remote-desktop-session
 ln -sf $PWD/chrome-remote-desktop /opt/google/chrome-remote-desktop/chrome-remote-desktop
+ln -sf $PWD/st-settings $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sf $PWD/st-packages "$HOME/.config/sublime-text-3/Packages/User/Package Control.sublime-settings"
