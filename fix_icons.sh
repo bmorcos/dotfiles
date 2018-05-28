62d0c9023b4d8c64942cf632bdee8ea7441ccc6a
bmorcos <morcos.ben@gmail.com>
2018-05-28 16:30:39 -0400

    Bug fix

--- fix_icons.sh
+++ fix_icons.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo sed -e "s/dropbox-client-.*=/dropbox-client-$(ps aux | grep dropbox | grep dist | awk '{print $2 }')=/g" -i ordering-override.keyfile
+sudo sed -e "s/dropbox-client-[0-9]*=/dropbox-client-$(ps aux | grep dropbox |grep dist | awk '{print $2 }')=/g" -i ~/.dotfiles/ordering-override.keyfile
 restart unity-panel-service
 
 # To get a list of applications dbus-send --type=method_call --print-reply --dest=com.canonical.indicator.application /com/canonical/indicator/application/service com.canonical.indicator.application.service.GetApplications | grep "object path" | sed 's/_/-/g' | cut -d"/" -f5
