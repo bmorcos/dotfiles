86308a475bd93c2277377dc7647ee18b139d28d2
bmorcos <morcos.ben@gmail.com>
2021-04-24 12:40:56 -0400

    Revert stream_audio script to template

--- stream_audio.sh
+++ stream_audio.sh
@@ -10,52 +10,69 @@
 #
 # Run with "-reset" to unload the new audio modules we created
 
-fname="/tmp/audio_modules.txt"
-
-if [ "$1" == "-reset" ]; then
-    while read line; do
-        pactl unload-module $line
-        echo "Unloaded module $line"
-    done < $fname
-    exit
-fi
+# fname="/tmp/audio_modules.txt"
+# 
+# if [ "$1" == "-reset" ]; then
+#     while read line; do
+#         pactl unload-module $line
+#         echo "Unloaded module $line"
+#     done < $fname
+#     exit
+# fi
 
 microphone="alsa_input.pci-0000_00_1f.3.analog-stereo"
 speakers="alsa_output.pci-0000_00_1f.3.analog-stereo"
 
 echo "Setting up echo cancellation"
-NUM=$(pactl load-module module-echo-cancel use_master_format=1 aec_method=webrtc \
+pactl load-module module-echo-cancel use_master_format=1 aec_method=webrtc \
       aec_args="analog_gain_control=0\\ digital_gain_control=1\\ experimental_agc=1\\ noise_suppression=1\\ voice_detection=1\\ extended_filter=1" \
       source_master="$microphone" source_name=src_ec  source_properties=device.description=src_ec \
-      sink_master="$speakers"     sink_name=sink_main
-      sink_properties=device.description=sink_main)
-echo $NUM | tee $fname
+      sink_master="$speakers"     sink_name=sink_main sink_properties=device.description=sink_main
 
 echo "Creating virtual output devices"
-NUM=$(pactl load-module module-null-sink sink_name=sink_fx
-sink_properties=device.description=sink_fx)
-echo $NUM | tee -a $fname
-
-NUM=$(pactl load-module module-null-sink sink_name=sink_mix
-sink_properties=device.description=sink_mix)
-echo $NUM | tee -a $fname
+pactl load-module module-null-sink sink_name=sink_fx  sink_properties=device.description=sink_fx
+pactl load-module module-null-sink sink_name=sink_mix sink_properties=device.description=sink_mix
 
 echo "Creating remaps"
-NUM=$(pactl load-module module-remap-source master=sink_mix.monitor \
-      source_name=src_main source_properties="device.description=src_main")
-echo $NUM | tee -a $fname
+pactl load-module module-remap-source master=sink_mix.monitor \
+      source_name=src_main source_properties="device.description=src_main"
 
 echo "Setting default devices"
 pactl set-default-source src_main
 pactl set-default-sink   sink_main
 
 echo "Creating loopbacks"
-NUM=$(pactl load-module module-loopback latency_msec=60 adjust_time=6 source=src_ec
-sink=sink_mix)
-echo $NUM | tee -a $fname
-NUM=$(pactl load-module module-loopback latency_msec=60 adjust_time=6
-source=sink_fx.monitor sink=sink_mix)
-echo $NUM | tee -a $fname
-NUM=$(pactl load-module module-loopback latency_msec=60 adjust_time=6
-source=sink_fx.monitor sink=sink_main)
-echo $NUM | tee -a $fname
+pactl load-module module-loopback latency_msec=60 adjust_time=6 source=src_ec          sink=sink_mix
+pactl load-module module-loopback latency_msec=60 adjust_time=6 source=sink_fx.monitor sink=sink_mix
+pactl load-module module-loopback latency_msec=60 adjust_time=6 source=sink_fx.monitor sink=sink_main
+
+# echo "Setting up echo cancellation"
+# NUM=$(pactl load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args="analog_gain_control=0\\ digital_gain_control=1\\ experimental_agc=1\\ noise_suppression=1\\ voice_detection=1\\ extended_filter=1" source_master="$microphone" source_name=src_ec  source_properties=device.description=src_ec sink_master="$speakers" sink_name=sink_main sink_properties=device.description=sink_main)
+# echo $NUM | tee $fname
+# 
+# echo "Creating virtual output devices"
+# NUM=$(pactl load-module module-null-sink sink_name=sink_fx
+# sink_properties=device.description=sink_fx)
+# echo $NUM | tee -a $fname
+# 
+# NUM=$(pactl load-module module-null-sink sink_name=sink_mix
+# sink_properties=device.description=sink_mix)
+# echo $NUM | tee -a $fname
+# 
+# echo "Creating remaps"
+# NUM=$(pactl load-module module-remap-source master=sink_mix.monitor source_name=src_main source_properties="device.description=src_main")
+# echo $NUM | tee -a $fname
+# 
+# echo "Setting default devices"
+# pactl set-default-source src_main
+# pactl set-default-sink   sink_main
+# 
+# echo "Creating loopbacks"
+# NUM=$(pactl load-module module-loopback latency_msec=60 adjust_time=6 source=src_ec sink=sink_mix)
+# echo $NUM | tee -a $fname
+# NUM=$(pactl load-module module-loopback latency_msec=60 adjust_time=6
+# source=sink_fx.monitor sink=sink_mix)
+# echo $NUM | tee -a $fname
+# NUM=$(pactl load-module module-loopback latency_msec=60 adjust_time=6
+# source=sink_fx.monitor sink=sink_main)
+# echo $NUM | tee -a $fname
